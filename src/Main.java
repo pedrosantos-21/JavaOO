8f4a6c8a049f8e288597be8883cd2089716cff51
pedro vinicius <128748644+pedrosantos-21@users.noreply.github.com>
2024-03-07 23:22:27 -0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Projetos da faculdade");
+        System.out.printf("Projetos da faculdade");
 
         }
     }
